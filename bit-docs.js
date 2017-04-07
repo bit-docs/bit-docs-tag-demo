eff1cb5c76f31ef639b1a72c4e397d15c8b9dd33
Joel Kuzmarski <leoj3n@gmail.com>
2017-04-07 07:58:58 -0500

    prelim docs

--- bit-docs.js
+++ bit-docs.js
@@ -1,5 +1,15 @@
 var tags = require("./tags");
 
+/**
+ * @module {function} bit-docs-tag-demo
+ * @parent plugins
+ *
+ * @description Provides an `@demo` tag for embedding HTML, CSS, and JS in an iframe.
+ *
+ * @body
+ *
+ * TBD
+ */
 module.exports = function(bitDocs){
 	var pkg = require("./package.json");
 	var deps = {};
