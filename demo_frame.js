63a6df7d6842989e9726606f7098ed90d3e1ca8b
imaustink <austin@austinkurpuis.com>
2017-04-13 08:52:58 -0700

    Auto scale demos
    Demo with now grow up to 600px when the content changes

--- demo_frame.js
+++ demo_frame.js
@@ -18,7 +18,7 @@ function render(node, docObject){
 	demoDiv.className = "demo";
 	demoDiv.innerHTML = template;
 	var demoSrc = (docObject.pathToRoot || "..") + "/" + (node.dataset ? node.dataset.demoSrc : node.getAttribute("data-demo-src"));
-	demoDiv.getElementsByTagName("iframe")[0].src = demoSrc;
+	var iframe = demoDiv.getElementsByTagName("iframe")[0].src = demoSrc;
 
 	node.innerHTML = "";
 	node.appendChild(demoDiv);
@@ -34,59 +34,68 @@ module.exports = function(node){
 	var iframe = node.getElementsByTagName("iframe")[0];
 
 	iframe.addEventListener("load", process);
+	iframe.contentWindow.window.addEventListener('resize', resizeIframe);
 
 	function process(){
-			var demoEl = this.contentDocument.getElementById('demo-html'),
-				sourceEl = this.contentDocument.getElementById('demo-source');
+		resizeIframe();
+		
+		var demoEl = this.contentDocument.getElementById('demo-html'),
+			sourceEl = this.contentDocument.getElementById('demo-source');
 
-			var html = getHTML.call(this, demoEl);
-			var js = getJS.call(this, sourceEl);
+		var html = getHTML.call(this, demoEl);
+		var js = getJS.call(this, sourceEl);
 
-			var dataForHtml = node.querySelector("[data-for=html] > pre");
-			dataForHtml.innerHTML = prettyify(html);
+		var dataForHtml = node.querySelector("[data-for=html] > pre");
+		dataForHtml.innerHTML = prettyify(html);
 
-			var dataForJS = node.querySelector("[data-for=js] > pre");
-			dataForJS.innerHTML = prettyify(js.replace(/\t/g,"  "));
-			show(node.querySelector("[data-tab=js]"));
+		var dataForJS = node.querySelector("[data-for=js] > pre");
+		dataForJS.innerHTML = prettyify(js.replace(/\t/g,"  "));
+		show(node.querySelector("[data-tab=js]"));
 
-			tabs();
+		tabs();
 	}
 
 	function getHTML(demoEl) {
-			var html = demoEl ? demoEl.innerHTML : this.contentWindow.DEMO_HTML;
+		var html = demoEl ? demoEl.innerHTML : this.contentWindow.DEMO_HTML;
 
-			if(!html) {
-				// try to make from body
-				var clonedBody = this.contentDocument.body.cloneNode(true);
-				var scripts = [].slice.call(clonedBody.getElementsByTagName("script"));
-				scripts.forEach(function(script){
-					if(!script.type || script.type.indexOf("javascript") === -1) {
-						script.parentNode.removeChild(script);
-					}
-				});
-				var styles = [].slice.call(clonedBody.getElementsByTagName("style"));
-				styles.forEach(function(style){
-					style.parentNode.removeChild(style);
-				});
-				html = clonedBody.innerHTML;
-			}
-			return html;
+		if(!html) {
+			// try to make from body
+			var clonedBody = this.contentDocument.body.cloneNode(true);
+			var scripts = [].slice.call(clonedBody.getElementsByTagName("script"));
+			scripts.forEach(function(script){
+				if(!script.type || script.type.indexOf("javascript") === -1) {
+					script.parentNode.removeChild(script);
+				}
+			});
+			var styles = [].slice.call(clonedBody.getElementsByTagName("style"));
+			styles.forEach(function(style){
+				style.parentNode.removeChild(style);
+			});
+			html = clonedBody.innerHTML;
+		}
+		return html;
 	}
 
 	function getJS(sourceEl){
-			var source = sourceEl ? sourceEl.innerHTML : this.contentWindow.DEMO_SOURCE;
-			if(!source){
-				var scripts = [].slice.call(this.contentDocument.querySelectorAll("script"));
-				// get the first one that is JS
-				for(var i =0; i < scripts.length; i++){
-					if(!scripts[i].type || (scripts[i].type.indexOf("javascript") >= 0 &&
-						!scripts[i].src)){
-						source =  scripts[i].innerHTML;
-						break;
-					}
+		var source = sourceEl ? sourceEl.innerHTML : this.contentWindow.DEMO_SOURCE;
+		if(!source){
+			var scripts = [].slice.call(this.contentDocument.querySelectorAll("script"));
+			// get the first one that is JS
+			for(var i =0; i < scripts.length; i++){
+				if(!scripts[i].type || (scripts[i].type.indexOf("javascript") >= 0 &&
+					!scripts[i].src)){
+					source =  scripts[i].innerHTML;
+					break;
 				}
 			}
-			return source.trim();
+		}
+		return source.trim();
+	}
+
+	function resizeIframe(){
+		var iframeHeight = iframe.contentWindow.document.body.scrollHeight;
+		iframe.style.height = Math.min(iframeHeight, 600) + 'px';
+		setTimeout(resizeIframe, 1000);
 	}
 
 	function show(el) {
