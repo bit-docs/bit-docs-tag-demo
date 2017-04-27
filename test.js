c700d5d5ebf5f0f01869b8b13e39292e4d4fa65c
Joel Kuzmarski <leoj3n@gmail.com>
2017-04-27 18:21:46 -0500

    Real tests using zombie

--- test.js
+++ test.js
@@ -1,105 +1,138 @@
-var assert = require("assert"),
-	generate = require("bit-docs-generate-html/generate"),
-	path = require("path");
-
-var Browser = require("zombie"),
+var path = require('path'),
+	rmrf = require('rimraf'),
+	assert = require('assert'),
+	Browser = require('zombie'),
 	express = require('express'),
-	testee = require('testee');
+	generate = require('bit-docs-generate-html/generate');
 
-Browser.localhost('127.0.0.1', 8081);
+Browser.localhost('*.example.com', 3003);
 
-var open = function(url, callback, done){
-	var server = express().use('/', express.static(__dirname + '/')).listen(8081);
-	var browser = new Browser();
-	browser.visit(url)
-		.then(function(){
-			callback(browser, function(){
-				server.close();
-			});
-		}).catch(function(e){
-			server.close();
-			done(e)
+describe('bit-docs-tag-demo', function () {
+	var server = express(),
+		browser = new Browser(),
+		temp = path.join(__dirname, 'temp');
+
+	before(function () {
+		return new Promise(function (resolve, reject) {
+			server = server.use('/', express.static(__dirname)).listen(3003, resolve);
+			server.on('error', reject);
 		});
-};
-
-describe("bit-docs-tag-demo", function(){
-
-	it("basics works", function (done) {
-		this.timeout(60000);
-
-		var docMap = Promise.resolve({
-			index: {
-				name: "index",
-				demo: "path/to/demo.html",
-				body: "<div class='demo_wrapper' data-demo-src='test/basics/demo.html'></div>\n"
-			}
-		});
-
-		generate(docMap, {
-			html: {
-				dependencies: {
-					"bit-docs-tag-demo": 'file:' + __dirname
-				}
-			},
-			dest: path.join(__dirname, "temp"),
-			parent: "index",
-			forceBuild: true,
-			debug: true,
-			minifyBuild: false
-		}).then(function () {
-
-			open("temp/index.html", function (browser, close) {
-				browser.assert.success();
-				browser.assert.element('section.body');
-
-				// https://github.com/stealjs/steal/issues/1177
-				// browser.assert.element('.demo');
-				// var doc = browser.window.document;
-				// var tabs = doc.getElementsByClassName("tab");
-				// assert.equal(tabs.length, 3, "there are 3 tabs");
-				// TODO better testing, click on stuff
-
-				close();
-				done();
-			}, done);
-
-		}, done);
 	});
 
-	it.only("client basics work", function(done) {
-		this.timeout(60000);
-
-		var docMap = Promise.resolve({
-			index: {
-				name: "index",
-				demo: "path/to/demo.html",
-				body: "<div class='demo_wrapper' data-demo-src='../basics/demo.html'></div>\n"
-			}
+	describe('temp directory', function () {
+		before(function (done) {
+			if (process.env.npm_config_generate == false) { this.skip(); }
+			rmrf(temp, done);
 		});
 
-		generate(docMap, {
-			html: {
-				dependencies: {
-					"bit-docs-tag-demo": 'file:' + __dirname,
+		it('is generated', function () {
+			this.timeout(60000);
+
+			var docMap = Promise.resolve({
+				index: {
+					name: "index",
+					body: "<div class='demo_wrapper' data-demo-src='test/basics/demo.html'></div>\n"
 				}
-			},
-			dest: path.join(__dirname, "test/resizes/generated-resizes-test"),
-			parent: "index",
-			forceBuild: true,
-			debug: true,
-			minifyBuild: false
-		}).then(function(){
-			testee.test(['test/resizes/resizes-test.html'], 'firefox', {
-				reporter: 'Spec'
-			}).then(function(results) {
-				console.log('123 results');
-				console.dir(results);
-				console.dir(results[0].tests);
-				results.forEach(function(result) {
-					assert.ok(result.passed, result.message);
-				});
-				done();
-			}, done);
-		}, done);
+			});
+
+			var siteConfig = {
+				html: {
+					dependencies: {
+						"bit-docs-tag-demo": 'file://' + __dirname
+					}
+				},
+				dest: temp,
+				debug: process.env.npm_config_debug,
+				devBuild: process.env.npm_config_devBuild,
+				parent: "index",
+				forceBuild: true,
+				minifyBuild: false
+			};
+
+			return generate(docMap, siteConfig);
+		});
+	});
+
+	describe('demo widget', function () {
+		before(function () {
+			return browser.visit('/temp/index.html');
+		});
+
+		it('exists on page', function () {
+			browser.assert.success();
+			browser.assert.element('.demo_wrapper', 'wrapper exists');
+			browser.assert.element('.demo_wrapper .demo', 'injected into wrapper');
+		});
+
+		describe('tabs and contents', function () {
+			it('has three', function () {
+				browser.assert.elements('.tab', 3, 'there are three tabs');
+				browser.assert.elements('.tab-content', 3, 'there are three tab contents');
+			});
+
+			it('only one active', function () {
+				browser.assert.element('.tab.active', 'only one tab is active');
+				browser.assert.element('.tab-content[style*="block"]', 'only one tab content is visible');
+			});
+
+			it('defaults to demo', function () {
+				browser.assert.text('.tab.active', 'Demo', 'Demo is active tab text');
+				browser.assert.attribute('.tab.active', 'data-tab', 'demo', 'demo is active data-tab');
+				browser.assert.style('[data-for="demo"]', 'display', 'block', 'demo tab content is visible');
+			});
+		});
+
+		describe('clicking HTML tab', function () {
+			before(function () {
+				return browser.click('[data-tab="html"]');
+			});
+
+			it('changes active tab and content', function () {
+				browser.assert.attribute('.tab.active', 'data-tab', 'html', 'html is active data-tab');
+				browser.assert.style('[data-for="html"]', 'display', 'block', 'html tab content is visible');
+			});
+		});
+	});
+
+	after(function () {
+		browser.destroy();
+		server.close();
 	});
 });
+
+	// it.only("client basics work", function(done) {
+	// 	this.timeout(60000);
+
+	// 	var docMap = Promise.resolve({
+	// 		index: {
+	// 			name: "index",
+	// 			demo: "path/to/demo.html",
+	// 			body: "<div class='demo_wrapper' data-demo-src='../basics/demo.html'></div>\n"
+	// 		}
+	// 	});
+
+	// 	generate(docMap, {
+	// 		html: {
+	// 			dependencies: {
+	// 				"bit-docs-tag-demo": 'file:' + __dirname,
+	// 			}
+	// 		},
+	// 		dest: path.join(__dirname, "test/resizes/generated-resizes-test"),
+	// 		parent: "index",
+	// 		forceBuild: true,
+	// 		debug: true,
+	// 		minifyBuild: false
+	// 	}).then(function(){
+	// 		testee.test(['test/resizes/resizes-test.html'], 'firefox', {
+	// 			reporter: 'Spec'
+	// 		}).then(function(results) {
+	// 			console.log('123 results');
+	// 			console.dir(results);
+	// 			console.dir(results[0].tests);
+	// 			results.forEach(function(result) {
+	// 				assert.ok(result.passed, result.message);
+	// 			});
+	// 			done();
+	// 		}, done);
+	// 	}, done);
+	// });
