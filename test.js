c359ecfdbf94e928808f3bdae1fab73da160d448
Joel Kuzmarski <leoj3n@gmail.com>
2017-04-28 02:40:40 -0500

    More comprehensive tests

--- test.js
+++ test.js
@@ -13,6 +13,7 @@ describe('bit-docs-tag-demo', function () {
 		temp = path.join(__dirname, 'temp');
 
 	before(function () {
+		if (!!process.env.npm_config_debug) { browser.debug(); }
 		return new Promise(function (resolve, reject) {
 			server = server.use('/', express.static(__dirname)).listen(3003, resolve);
 			server.on('error', reject);
@@ -21,17 +22,30 @@ describe('bit-docs-tag-demo', function () {
 
 	describe('temp directory', function () {
 		before(function (done) {
-			if (process.env.npm_config_generate == false) { this.skip(); }
+			if (!!process.env.npm_config_skipGenerate) { this.skip(); }
 			rmrf(temp, done);
 		});
 
 		it('is generated', function () {
 			this.timeout(60000);
 
+			function demo_wrapper(path) {
+				return '<div class="demo_wrapper" data-demo-src="test/basics/' + path + '.html"></div>' + "\n"
+			}
+
 			var docMap = Promise.resolve({
-				index: {
+				withIds: {
+					name: "withIds",
+					body: demo_wrapper('demo-with-ids')
+				}, withoutIds: {
+					name: "withoutIds",
+					body: demo_wrapper('demo-without-ids')
+				}, withoutJs: {
+					name: "withoutJs",
+					body: demo_wrapper('demo-without-js')
+				}, index: {
 					name: "index",
-					body: "<div class='demo_wrapper' data-demo-src='test/basics/demo.html'></div>\n"
+					body: demo_wrapper('demo-with-ids') + demo_wrapper('demo-without-ids') + demo_wrapper('demo-without-js')
 				}
 			});
 
@@ -42,8 +56,8 @@ describe('bit-docs-tag-demo', function () {
 					}
 				},
 				dest: temp,
-				debug: process.env.npm_config_debug,
-				devBuild: process.env.npm_config_devBuild,
+				debug: !!process.env.npm_config_debug,
+				devBuild: !!process.env.npm_config_devBuild,
 				parent: "index",
 				forceBuild: true,
 				minifyBuild: false
@@ -54,42 +68,96 @@ describe('bit-docs-tag-demo', function () {
 	});
 
 	describe('demo widget', function () {
-		before(function () {
-			return browser.visit('/temp/index.html');
-		});
-
-		it('exists on page', function () {
-			browser.assert.success();
-			browser.assert.element('.demo_wrapper', 'wrapper exists');
-			browser.assert.element('.demo_wrapper .demo', 'injected into wrapper');
-		});
-
-		describe('tabs and contents', function () {
-			it('has three', function () {
-				browser.assert.elements('.tab', 3, 'there are three tabs');
-				browser.assert.elements('.tab-content', 3, 'there are three tab contents');
+		function basicsWork() {
+			it('exists on page', function () {
+				browser.assert.success();
+				browser.assert.element('.demo_wrapper', 'wrapper exists');
+				browser.assert.element('.demo_wrapper .demo', 'injected into wrapper');
 			});
 
-			it('only one active', function () {
-				browser.assert.element('.tab.active', 'only one tab is active');
-				browser.assert.element('.tab-content[style*="block"]', 'only one tab content is visible');
+			describe('tabs and contents', function () {
+				it('has three', function () {
+					browser.assert.elements('.tab', 3, 'there are three tabs');
+					browser.assert.elements('.tab-content', 3, 'there are three tab contents');
+				});
+
+				it('only one active', function () {
+					browser.assert.element('.tab.active', 'only one tab is active');
+					browser.assert.element('.tab-content:not([style*="none"])', 'only one tab content is visible');
+				});
+
+				it('defaults to demo', function () {
+					browser.assert.text('.tab.active', 'Demo', 'Demo is active tab text');
+					browser.assert.attribute('.tab.active', 'data-tab', 'demo', 'demo is active data-tab');
+					browser.assert.style('[data-for="demo"]', 'display', '', 'demo tab content is visible');
+				});
 			});
 
-			it('defaults to demo', function () {
-				browser.assert.text('.tab.active', 'Demo', 'Demo is active tab text');
-				browser.assert.attribute('.tab.active', 'data-tab', 'demo', 'demo is active data-tab');
-				browser.assert.style('[data-for="demo"]', 'display', 'block', 'demo tab content is visible');
-			});
-		});
+			describe('clicking HTML tab', function () {
+				before(function () {
+					return browser.click('[data-tab="html"]');
+				});
 
-		describe('clicking HTML tab', function () {
+				it('changes tab and content', function () {
+					browser.assert.attribute('.tab.active', 'data-tab', 'html', 'html is active data-tab');
+					browser.assert.style('[data-for="html"]', 'display', '', 'html tab content is visible');
+				});
+			});
+		}
+
+		describe('with ids', function () {
 			before(function () {
-				return browser.click('[data-tab="html"]');
+				return browser.visit('/temp/withIds.html');
 			});
 
-			it('changes active tab and content', function () {
-				browser.assert.attribute('.tab.active', 'data-tab', 'html', 'html is active data-tab');
-				browser.assert.style('[data-for="html"]', 'display', 'block', 'html tab content is visible');
+			basicsWork();
+		});
+
+		describe('without ids', function () {
+			before(function () {
+				return browser.visit('/temp/withoutIds.html');
+			});
+
+			basicsWork();
+		});
+
+		describe('without js', function () {
+			before(function () {
+				return browser.visit('/temp/withoutJs.html');
+			});
+
+			basicsWork();
+
+			describe('JS tab', function () {
+				it('is hidden', function () {
+					browser.assert.style('[data-tab="js"]', 'display', 'none', 'js tab content is hidden');
+				});
+			});
+		});
+
+		describe('multiple instances', function () {
+			before(function () {
+				return browser.visit('/temp/index.html');
+			});
+
+			it('exist on page', function () {
+				browser.assert.success();
+				browser.assert.elements('.demo_wrapper', 3, 'three wrappers exists');
+				browser.assert.elements('.demo_wrapper .demo', 3, 'three injected into wrappers');
+			});
+
+			describe('clicking all HTML tabs', function () {
+				before(function () {
+					var htmlTabs = browser.queryAll('[data-tab="html"]');
+					return Promise.all(htmlTabs.map(function(el) {
+						browser.click(el);
+					}));
+				});
+
+				it('changes tabs and contents', function () {
+					browser.assert.attribute('.tab.active', 'data-tab', 'html', 'html is active data-tab');
+					browser.assert.style('[data-for="html"]', 'display', '', 'html tab content is visible');
+				});
 			});
 		});
 	});
@@ -99,40 +167,3 @@ describe('bit-docs-tag-demo', function () {
 		server.close();
 	});
 });
-
-	// it.only("client basics work", function(done) {
-	// 	this.timeout(60000);
-
-	// 	var docMap = Promise.resolve({
-	// 		index: {
-	// 			name: "index",
-	// 			demo: "path/to/demo.html",
-	// 			body: "<div class='demo_wrapper' data-demo-src='../basics/demo.html'></div>\n"
-	// 		}
-	// 	});
-
-	// 	generate(docMap, {
-	// 		html: {
-	// 			dependencies: {
-	// 				"bit-docs-tag-demo": 'file:' + __dirname,
-	// 			}
-	// 		},
-	// 		dest: path.join(__dirname, "test/resizes/generated-resizes-test"),
-	// 		parent: "index",
-	// 		forceBuild: true,
-	// 		debug: true,
-	// 		minifyBuild: false
-	// 	}).then(function(){
-	// 		testee.test(['test/resizes/resizes-test.html'], 'firefox', {
-	// 			reporter: 'Spec'
-	// 		}).then(function(results) {
-	// 			console.log('123 results');
-	// 			console.dir(results);
-	// 			console.dir(results[0].tests);
-	// 			results.forEach(function(result) {
-	// 				assert.ok(result.passed, result.message);
-	// 			});
-	// 			done();
-	// 		}, done);
-	// 	}, done);
-	// });
